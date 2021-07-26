85b3b6b0089439acb9d9435a36f7d1f77329dbea
dayanadocheva <dayana.docheva.a28@learn.telerikacademy.com>
2021-07-26 10:26:54 +0300

    Change the image path in Selenium

--- Automation/Selenium WebDriver Test Automation/WEare Social Network/src/test/java/stepDefinitions/BaseStepDefinitions.java
+++ Automation/Selenium WebDriver Test Automation/WEare Social Network/src/test/java/stepDefinitions/BaseStepDefinitions.java
@@ -7,12 +7,7 @@ import pages.weAreSocialNetwork.*;
 
 public class BaseStepDefinitions {
     protected static UserActions actions=new UserActions();
-    protected RegisterPage registrationPage=new RegisterPage(actions.getDriver());
     protected LoginPage loginPage=new LoginPage(actions.getDriver());
-    protected SearchPage searchPage=new SearchPage(actions.getDriver());
-    protected PersonalProfileEditorPage personalProfileEditorPage=new PersonalProfileEditorPage(actions.getDriver());
-    protected LogOutPage logOutPage=new LogOutPage(actions.getDriver());
-
 
     @BeforeStories
     public void setUp() {
